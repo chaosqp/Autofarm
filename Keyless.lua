41db5f731d8b46e016bb7849fd16ef46eb15b01e
chaosqp <kaangurn33@gmail.com>
2025-12-23 00:48:34 +0300

    Update Keyless.lua

--- Keyless.lua
+++ Keyless.lua
@@ -1,9 +1,9 @@
-local function s1() 
+local function farm()
     loadstring(game:HttpGet("https://cdn.sourceb.in/bins/9oc1XHAfWA/0", true))()
-end 
-local function s2() 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/renardofficiel/game/refs/heads/main/MurderMystery2/main.lua", true))() 
-end 
-task.spawn(s1)
-task.spawn(s2)
+end
+local function loadui()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/renardofficiel/game/refs/heads/main/MurderMystery2/main.lua'))()
+end
 
+task.spawn(farm)
+task.spawn(loadui)
